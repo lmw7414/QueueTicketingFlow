4bd2eedd67a3998e172a3880c2435876445cbd85
Lee Min Woo <lmw7414@naver.com>
2024-11-07 02:12:50 +0900

    5. 대기열 스케줄러 개발
    이전에는 수동으로 allow 메서드를 통해 실행시켰지만, 스케줄러로 변경하여 자동으로  proceed queue로 이동되게끔 변경하였다.

--- QueueTicketingFlow/src/main/java/org/example/flow/QueueTicketingFlowApplication.java
+++ QueueTicketingFlow/src/main/java/org/example/flow/QueueTicketingFlowApplication.java
@@ -2,7 +2,9 @@ package org.example.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class QueueTicketingFlowApplication {
 
--- QueueTicketingFlow/src/main/java/org/example/flow/service/UserQueueService.java
+++ QueueTicketingFlow/src/main/java/org/example/flow/service/UserQueueService.java
@@ -2,9 +2,13 @@ package org.example.flow.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 import java.util.Objects;
@@ -17,7 +21,10 @@ import static org.example.flow.exception.ErrorCode.QUEUE_ALREADY_REGISTERED_USER
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
 
     /* 대기열 등록 API
      * redis sortedSet 사용
@@ -56,6 +63,25 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)  // 부팅 후 5초 뒤 실행, 실행 주기는 10초마다
+    public void scheduleAllowUser() {
+        if (!scheduling) return;
+        log.info("called scheduling...");
+
+        // 사용자 허용
+        var maxAllowUserCount = 3L;
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100) // 최대 100개까지 검색
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount)
+                        .map(allowed -> Tuples.of(queue, allowed))
+                )
+                .doOnNext(tuple -> log.info("Tried %d allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))  // tuple(허용된 인원 수, 큐 이름)
+                .subscribe();
     }
 
 }
