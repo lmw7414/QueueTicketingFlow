b979e7a1a2cfe2c91e9847a040037b3dbd94908f
Lee Min Woo <lmw7414@naver.com>
2024-11-07 00:21:37 +0900

    1. 프로젝트 초기 설정-의존성 추가
    Spring boot : 3.3.5
    Java 17
    
    1) web site 프로젝트
      - thymeleaf
    2) Flow 프로젝트 Dependency 추가
      - redis-reactive
      - thymeleaf
      - webflux

---
+++ QueueTicketingFlow/src/main/java/org/example/flow/QueueTicketingFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueTicketingFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueTicketingFlowApplication.class, args);
+    }
+
+}
---
+++ QueueTicketingFlow/src/test/java/org/example/flow/QueueTicketingFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueTicketingFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
